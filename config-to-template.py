5c7c37185a6e5c023a238f40f537d5a7bb056cae
Justin Caringal <jacfriday10th@gmail.com>
2023-08-10 01:47:42 -0500

    Finished rudimentary template config generator
    -Will add flag features in the future

--- config-to-template.py
+++ config-to-template.py
@@ -18,6 +18,13 @@ l.setLevel(logging.INFO)
 debug = l.debug; info = l.info; warning = l.warning; error = l.error
 
 DESCRIPTION = '''
+
+A tool to take a config.json file and outputs a template
+
+JSON file that takes out all of the sensitive information
+
+and replaces it with data types. Works with any JSON file
+
 '''
 
 EPILOG = '''
@@ -41,9 +48,42 @@ if args.verbose:
   l.setLevel(logging.DEBUG)
 
 
+# extracts variable type
+def extract_type(value):
+  var_type = str(type(value))
+  start_indx = var_type.index('\'') + 1 # finds first '
+  end_indx = var_type.index('\'', start_indx) # finds second '
+  extracted_var_type = var_type[start_indx : end_indx]
+  return extracted_var_type
+
 
 debug('%s begin', SCRIPT_PATH)
 
+
+# opens template file into dict
+with open(args.config, 'r') as config:
+  config_dict = json.loads(config.read())
+
+# iterates through dict
+for key, value in config_dict.items():
+  
+  # extracts variable type
+  var_type = extract_type(value)
+  debug(f'{key}\t\t\t{value}\t\t\t{var_type}')
+
+  # replaces value at key with var_type
+  config_dict[key] = var_type
+
+# extracts extension, generates new file name
+basename = os.path.basename(args.config)
+extension_indx = basename.rindex('.')
+extension = basename[extension_indx : ]
+debug(f'Extension - {extension}')
+template_name = basename.replace(extension, f'.template{extension}')
+
+# creates new template json file
+with open(template_name, 'w') as template:
+  template.write(json.dumps(config_dict, indent = 4))
 
 
 debug('%s end', SCRIPT_PATH)
